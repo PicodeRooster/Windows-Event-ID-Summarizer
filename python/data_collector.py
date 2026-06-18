e0379013082b64b85670cd8969748dfe6027a139
PicodeRooster <colettemoreau30@gmail.com>
2026-06-17 22:23:36 -0400

    working on content build

--- python/data_collector.py
+++ python/data_collector.py
@@ -1,12 +1,32 @@
 from urllib.request import urlopen
+
 url = "https://www.ultimatewindowssecurity.com/securitylog/encyclopedia/event.aspx?eventid=1105"
+
 page = urlopen(url)
 html_bytes = page.read()
 html = html_bytes.decode("utf-8")
 
-h2_index = html.find("<h2>")
-start_index = h2_index + len("<h2>")
-end_index = html.find("</h2>")
-h2 = html[start_index:end_index]
+# --- Event Title ---
 
-print(h2)
+log_title_index = html.find('<p class="hey">')
+log_title_start_index = log_title_index + len('<p class="hey">')
+log_title_end_index = html.find("</p>")
+log_title = html[log_title_start_index:log_title_end_index].strip()
+
+# --- Description Paragraph --- 
+
+p_index = html.index('</p>')
+
+print(log_title)
+print(p_index)
+
+#print(span) // Windows Security Log Event ID 1105
+# <div id="contentMargin">
+
+'''
+<p class="hey">
+        1105: Event log automatic backup
+    </p>
+
+    <p>
+'''
